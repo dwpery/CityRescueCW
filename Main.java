cea1bdc874bc2eacd82b698d941c7f86b9a7fcc9
Samuel Bartle <sb1629@exeter.ac.uk>
2026-02-24 13:22:21 +0000

    main files

---
+++ Main.java
@@ -0,0 +1,64 @@
+
+public class Main {
+
+    public enum IncidentType {
+    MEDICAL,
+    FIRE,
+    CRIME
+    }
+
+    public enum UnitType {
+    AMBULANCE,
+    FIRE_ENGINE,
+    POLICE_CAR
+    }
+
+    public enum IncidentStatus {
+    REPORTED,
+    DISPATCHED,
+    IN_PROGRESS,
+    RESOLVED,
+    CANCELLED
+    }
+
+    public enum UnitStatus {
+    IDLE,
+    EN_ROUTE,
+    AT_SCENE,
+    OUT_OF_SERVICE
+    }
+
+    class Incident {
+
+        
+
+
+    }
+
+    class Unit {
+
+        
+
+
+    }
+
+    class Ambulance extends Unit{
+
+    }
+
+    class FireEngine extends Unit {
+
+    }
+    class PoliceCar extends Unit {
+
+    }
+
+
+
+    public static void main(String[] args){
+        
+        
+
+    }
+    
+}
